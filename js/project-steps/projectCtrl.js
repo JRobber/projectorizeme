1b6b48fb140e407e9e3fdc670f0adb07e6df35e1
Jeremy R <jeremy@devmounta.in>
2016-01-08 16:11:16 -0700

    Back to starting point

--- js/project-steps/projectCtrl.js
+++ js/project-steps/projectCtrl.js
@@ -1,18 +1,3 @@
-angular.module('projectorize').controller('projectCtrl', function($scope, projectService){
-    $scope.isLoading = true;
+angular.module('projectorize').controller('projectCtrl', function($scope){
     
-    $scope.getProject = function(){
-        projectService.getProject().then(function(data){
-            $scope.isLoading = false;
-            
-            $scope.project = data;  
-            $scope.currentPart = $scope.project.parts[0];
-        })
-    }
-    
-    $scope.getProject();
-    
-    $scope.setCurrentPart = function(part) {
-        $scope.currentPart = part;
-    }
 })
--- js/project-steps/projectService.js
+++ js/project-steps/projectService.js
@@ -1,22 +1,3 @@
-angular.module('projectorize').service('projectService', function($http, $q, $timeout){
-    
-    this.getProject = function() {
-        
-        var defer = $q.defer();
-        
-        $http({
-            method: 'GET',
-            url: '/api/project'
-        }).then(function(response){
-            if(response.status === 200){
-                $timeout(function(){
-                    defer.resolve(response.data);
-                }, 3000)
-            }
-            return "Error: All the things are bad!"
-        })
-        
-        return defer.promise;
-    }
+angular.module('projectorize').service('projectService', function(){
     
 })
